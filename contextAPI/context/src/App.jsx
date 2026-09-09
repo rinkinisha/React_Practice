22b40b24595d139f35ee66d4b202e517c5973bcd
Rinki9999 <rinkinisha24@navgurukul.org>
2026-09-09 20:33:15 +0530

    new changes

--- contextAPI/context/src/App.jsx
+++ contextAPI/context/src/App.jsx
@@ -1,4 +1,5 @@
 import Counter from "./components/Counter"
+import Todo from "./components/Todo"
 
 function App(){
   
@@ -7,6 +8,7 @@ function App(){
   
 <Counter
 />
+<Todo/>
     </>
   )
 }
--- contextAPI/context/src/components/Todo.jsx
+++ contextAPI/context/src/components/Todo.jsx
@@ -1,10 +1,57 @@
-import react from "react"
+import react, { useState } from "react"
 function Todo(){
+    const [todo,setTodo]=useState("")
+    const [todos , setTodos]=useState([])
+    const [editTodos , seteditTodos]=useState(null)
+    function Addtodo(){
+      setTodos([...todos,todo])
+      setTodo("")
+    }
+    function handlechange(e){
+     setTodo(e.target.value)
+    }
+
+    function deletetodo(index){
+        const newtodo=todos.filter((item,i)=> i !== index)
+        setTodos(newtodo)
+    }
+
+    function editTodo(index){
+     setTodo(todos[index])
+     seteditTodos(index)
+    }
+    function updatedTodos(){
+        const updatedtask=[...todos]
+        updatedtask[editTodos]=todo
+        
+
+    }
 return(
     <>
+    <h1>Todo</h1>
+    <input
+    type="text"
+    value={todo}
+    onChange={handlechange}
+    placeholder="Write something......"/>
     
+    <button onClick={editTodos === null ?Addtodo : updateTodo}>{editTodo===null?"ADD":"UPDATE"}</button>
+    {
+    
+        todos.map((item , index)=>(
+            <>
+        <div key={index}>{item}</div>
+         <button onClick={()=>{deletetodo(index)}}>Delete</button>
+         <button onClick={()=>editTodo(index)}>Edit</button>
+         </>
+        )
+        ) 
+}  
+
     </>
 )
 
 }
 export default Todo
+
+
